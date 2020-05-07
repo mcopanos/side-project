b339761c74dcbf1b73c936ee93fda90a01541f41
Mike Copanos <mcopanos@gmail.com>
2020-05-06 22:02:07 -0500

    Initial commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,13 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-
-mix
-    .js('assets/js/main.js', 'web/js/main.js')
-    .sass('assets/sass/app.scss', 'web/css')
+mix.version()
+    .disableNotifications()
+    .setPublicPath('web')
+    .js('assets/js/main.js', 'web/js/')
+    .sass('assets/sass/app.scss', 'web/css/app.css')
+    .copyDirectory('assets/css', 'web/css')
+    .sourceMaps()
+    .browserSync({
+        proxy: 'localhost:8080',
+        files: ['templates/**/*', 'web/css/*.css', 'web/js/*.js']
+    })
